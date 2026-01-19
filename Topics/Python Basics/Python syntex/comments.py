a5fe318d63b6b4199f23d721004fd7fd41d0dd70
Sapra Anand Jagdishbhai <152870282+Sapra007@users.noreply.github.com>
2026-01-19 15:07:46 +0530

    Add files via upload

---
+++ Topics/Python Basics/Python syntex/comments.py
@@ -0,0 +1,10 @@
+#This is a comment.
+print("Hello, World!")
+#Comments are ignored by Python.
+
+
+
+"""This is a
+multiline docstring."""
+print("Hello, World!")
+#Docstrings can also be used as comments.
